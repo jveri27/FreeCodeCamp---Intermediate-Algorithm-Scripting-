30fec2f47b0f3fafbc37c8dc44e809b2a497deba
Junel Veri <junelveri@gmail.com>
2015-12-07 22:47:04 +0800

    Update Search and Replace.js

--- Search and Replace.js
+++ Search and Replace.js
@@ -1,4 +1,4 @@
-Bonfire: Search and Replace
+/*Bonfire: Search and Replace
 
 Perform a search and replace on the sentence using the arguments provided and return the new sentence.
 
@@ -9,7 +9,7 @@ Second argument is the word that you will be replacing (before).
 Third argument is what you will be replacing the second argument with (after).
 
 NOTE: Preserve the case of the original word when you are replacing it. For example if you mean to replace the word "Book" with the word "dog", it should be replaced as "Dog"
-
+*/
 function myReplace(str, before, after) {
   var toBeRep = before;
   var str2;
