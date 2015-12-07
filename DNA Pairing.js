55ca050a05df88fd576c25cb46a3dd30100e5e22
Junel Veri <junelveri@gmail.com>
2015-12-07 22:33:20 +0800

    Update DNA Pairing.js

--- DNA Pairing.js
+++ DNA Pairing.js
@@ -1,3 +1,16 @@
+/*Bonfire: DNA Pairing
+
+The DNA strand is missing the pairing element. Take each character, get its pair, and return the results as a 2d array.
+
+Base pairs are a pair of AT and CG. Match the missing element to the provided character.
+
+Return the provided character as the first element in each array.
+
+For example, for the input GCG, return [["G", "C"], ["C","G"],["G", "C"]]
+
+The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array.
+*/
+
 //initialize DNA strand pairs
   var arr = ['A', 'T', 'C', 'G'];
   var equiArr = ['T', 'A', 'G', 'C'];
